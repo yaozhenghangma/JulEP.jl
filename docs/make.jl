b27b813e28e2b0019d166e0c7aa1793d9affaa6b
Yaozhenghang Ma <yaozhenghang.ma@gmail.com>
2022-04-05 12:33:06 +0800

    update

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(MatterEnv, :DocTestSetup, :(using MatterEnv); recursive=true
 makedocs(;
     modules=[MatterEnv],
     authors="Yaozhenghang Ma <yaozhenghang.ma@gmail.com> and contributors",
-    repo="https://github.com/YaozhenghangMa/MatterEnv.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/yaozhenghangma/MatterEnv.jl/blob/{commit}{path}#{line}",
     sitename="MatterEnv.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://YaozhenghangMa.github.io/MatterEnv.jl",
+        canonical="https://yaozhenghangma.github.io/MatterEnv.jl",
         assets=String[],
     ),
     pages=[
@@ -19,5 +19,5 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/YaozhenghangMa/MatterEnv.jl",
+    repo="github.com/yaozhenghangma/MatterEnv.jl",
 )
